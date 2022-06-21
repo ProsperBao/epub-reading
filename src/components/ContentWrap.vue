dadb33659c5737f53fcb89144184fecc8bab9ff7
FuBaooo <baihe2233@gmail.com>
2022-06-21 21:26:20 +0800

    feat: 修改

---
+++ src/components/ContentWrap.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+defineProps<{ line: string }>()
+</script>
+
+<template>
+  <div />
+</template>
---
+++ src/components/ContentWrapLine.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+defineProps<{ lineHtml: string }>()
+</script>
+
+<template>
+  <div />
+</template>
