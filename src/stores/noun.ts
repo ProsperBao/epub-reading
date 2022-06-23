9abfa3e63457856eda3cbe493a5ec34daa1c988f
FuBaooo <baihe2233@gmail.com>
2022-06-23 17:07:53 +0800

    feat: separate noun list

--- src/stores/noun.ts
+++ src/stores/noun.ts
@@ -6,15 +6,22 @@ export type UniqueNounList = Record<string, UniqueNounMap>
 export type UniqueNounMap = Record<string, string>
 
 // 特有名词
-export const useUniqueNounStore = defineStore('noun', () => ({
-  use: useLocalStorage<string[]>('use', [
-    '本好きの下剋上',
-  ]),
-  // 和每个小说/自定义词典一样，每个小说/自定义词典都有一个独立的列表
-  list: useLocalStorage<UniqueNounList>('list', {
-    本好きの下剋上: {
-      ランプレヒト: '兰普雷特',
-      冬の主: '冬之主',
-    },
-  }),
-}))
+export const useUniqueNounStore = defineStore('noun', () => {
+  const list = import.meta.globEager('~/noun/*.json')
+
+  return {
+    use: useLocalStorage<string[]>('use', [
+      '本好きの下剋上',
+    ]),
+    // 和每个小说/自定义词典一样，每个小说/自定义词典都有一个独立的列表
+    list: useLocalStorage<UniqueNounList>('list',
+      Object.entries(list).reduce((acc, [key, value]) => {
+        const name = key.split('/').pop()?.replace('.json', '')
+        if (name)
+          acc[name] = value.default
+
+        return acc
+      }, {} as UniqueNounList),
+    ),
+  }
+})
