183d022cc8d4992989f4487f264e571279070008
FuBaooo <baihe2233@gmail.com>
2022-06-23 16:50:42 +0800

    feat: translate

--- src/components/ContentWrap.vue
+++ src/components/ContentWrap.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import type { NormalizeStringify } from '~/utils/epub'
-
+import { translate } from '~/utils/translate'
 defineProps<{ content: NormalizeStringify[] }>()
 </script>
 
@@ -8,7 +8,7 @@ defineProps<{ content: NormalizeStringify[] }>()
   <section>
     <div text-left>
       <template v-for="line in content" :key="line.hash">
-        <p mt-4 v-html="line.origin" />
+        <p mt-4 @click="translate(line)" v-html="line.origin" />
         <p v-if="line.translate" text-sm mt-1>
           {{ line.translate }}
         </p>
--- src/composables/useEpub.ts
+++ src/composables/useEpub.ts
@@ -1,12 +1,14 @@
 import type { Book } from 'epubjs'
 import epub from 'epubjs'
 import type { Ref } from 'vue'
+import { useHistoryStore } from '~/stores'
 import type { NormalizeStringify } from '~/utils/epub'
 import { extractConvert2Dom, normalizeConvert } from '~/utils/epub'
 
 export function useEpub(epubData: Ref<ArrayBuffer | null>) {
   const book = ref<Book | null>(null)
   const html = ref<NormalizeStringify[]>([])
+  const { record } = useHistoryStore()
 
   // 加载 epub 文件
   const loadEpubFile = async (n: ArrayBuffer) => {
@@ -20,6 +22,10 @@ export function useEpub(epubData: Ref<ArrayBuffer | null>) {
   watch(() => epubData.value, n => n && loadEpubFile(n))
 
   return {
-    html,
+    html: computed(() => {
+      if (html.value.length === 0)
+        return []
+      return html.value.map(i => ({ ...i, translate: record[i.hash]?.translate || i.translate }))
+    }),
   }
 }
