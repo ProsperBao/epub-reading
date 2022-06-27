25a8d7e0c4be6e7a68117514d05ba07a5c075035
FuBaooo <baihe2233@gmail.com>
2022-06-27 11:41:42 +0800

    feat: scroll bottom load

--- src/components/ContentWrap.vue
+++ src/components/ContentWrap.vue
@@ -1,20 +1,30 @@
 <script lang="ts" setup>
 import type { NormalizeStringify } from '~/utils/epub'
 import { translate } from '~/utils/translate'
-defineProps<{ content: NormalizeStringify[] }>()
-defineEmits(['next'])
+const props = defineProps<{ content: NormalizeStringify[] }>()
+const emit = defineEmits(['next'])
+
+const target = ref(null)
+
+useIntersectionObserver(
+  target,
+  ([{ isIntersecting }]) => {
+    if (props.content.length > 0 && isIntersecting)
+      emit('next')
+  },
+)
 </script>
 
 <template>
   <section>
     <div text-left>
       <template v-for="line in content" :key="line.hash">
-        <p mt-4 @click="translate(line)" v-html="line.origin" />
-        <p v-if="line.translate" text-sm mt-1>
+        <p opacity-5 mt-4 @click="translate(line)" v-html="line.origin" />
+        <p v-if="line.translate" opacity-5 text-sm mt-1>
           {{ line.translate }}
         </p>
       </template>
-      <div h-5 bg-blue />
     </div>
+    <div ref="target" h-5 bg-blue />
   </section>
 </template>
